7825cc96ce70ce1f93c9105960a92e6c3e58af71
Amit Gupta <amit_@outlook.in>
2023-12-13 12:32:36 -0600

    Non-uniform cuda devices

--- MLModel/MLModel.cpp
+++ MLModel/MLModel.cpp
@@ -44,17 +44,22 @@ void PytorchModel::SetExecutionDevice(const char *const device_name) {
         MPI_Comm_size(MPI_COMM_WORLD, &size);
         // get number of cuda devices visible
         auto cuda_device_visible_env_var = std::getenv("CUDA_VISIBLE_DEVICES"); //input "0,1,2"
+        std::vector<std::string> cuda_device_visible_ids;
         int num_cuda_devices_visible = 0;
         if (cuda_device_visible_env_var != nullptr){
             std::string cuda_device_visible_env_var_str(cuda_device_visible_env_var);
             num_cuda_devices_visible = std::count(cuda_device_visible_env_var_str.begin(), cuda_device_visible_env_var_str.end(), ',') + 1;
+            for (int i = 0; i < num_cuda_devices_visible; i++) {
+                cuda_device_visible_ids.push_back(cuda_device_visible_env_var_str.substr(0, cuda_device_visible_env_var_str.find(',')));
+                cuda_device_visible_env_var_str.erase(0, cuda_device_visible_env_var_str.find(',') + 1);
+            }
         } else {
             throw std::invalid_argument("CUDA_VISIBLE_DEVICES not set\n "
                                         "You requested for manual MPI aware device allocation but CUDA_VISIBLE_DEVICES is not set\n");
         }
         // assign cuda device to ranks in round-robin fashion
         device_name_as_str += ":";
-        device_name_as_str += std::to_string(rank % num_cuda_devices_visible);
+        device_name_as_str += cuda_device_visible_ids[rank % num_cuda_devices_visible];
         char hostname[256];
         gethostname(hostname, 256);
         // poor man's sync print
